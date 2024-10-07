303e2007fc6743953725bd836649fcec12b9f48e
Tom <squid@stormwind.it>
2024-10-07 14:50:46 +0200

    feat(solution): add Eldergrove.Gui project to the solution for GUI development feat(gui): create Eldergrove.Gui project with initial setup and dependencies feat(gui): add Cheepicus12 font configuration and associated image file feat(gui): implement basic program structure for SadConsole application

---
+++ src/Eldergrove.Gui/Program.cs
@@ -0,0 +1,30 @@
+﻿using SadConsole;
+using SadConsole.Configuration;
+using SadRogue.Primitives;
+
+Settings.WindowTitle = "SadConsole Examples";
+
+
+// Configure how SadConsole starts up
+Builder startup = new Builder()
+        .SetScreenSize(90, 30)
+        .UseDefaultConsole()
+        .OnStart(Game_Started)
+        .IsStartingScreenFocused(true)
+        .ConfigureFonts(true)
+    ;
+
+// Setup the engine and start the game
+Game.Create(startup);
+Game.Instance.Run();
+Game.Instance.Dispose();
+
+void Game_Started(object? sender, GameHost host)
+{
+    ColoredGlyph boxBorder = new(Color.White, Color.Black, 178);
+    ColoredGlyph boxFill = new(Color.White, Color.Black);
+
+    Game.Instance.StartingConsole.FillWithRandomGarbage(255);
+    Game.Instance.StartingConsole.DrawBox(new Rectangle(2, 2, 26, 5), ShapeParameters.CreateFilled(boxBorder, boxFill));
+    Game.Instance.StartingConsole.Print(4, 4, "Welcome to SadConsole!");
+}
