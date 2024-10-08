03c1069ddbd073f5b38c4e6526e6e8195df954b3
Tom <tom@isendu.com>
2024-10-08 13:59:52 +0200

    refactor: consolidate color and tile definitions into index.js and index.ts for better organization and maintainability chore: remove unused color, tile, and name scripts to clean up the codebase

--- test_game/ts_sources/src/names.ts
+++
@@ -1,3 +0,0 @@
-name_add_from_file("female", "names/fantasy_female_names.txt" );
-name_add_from_file("male", "names/fantasy_male_names.txt");
-name_add_from_file("animal", "names/fantasy_animal_names.txt");
