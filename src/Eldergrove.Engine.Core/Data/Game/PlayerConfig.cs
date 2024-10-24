07bc505a4953d55b57da0fb081045d7146488434
Tom <squid@stormwind.it>
2024-10-24 19:29:29 +0200

    feat(PlayerConfig.cs): add StartingItems property to PlayerConfig class to store a list of starting items for players

--- src/Eldergrove.Engine.Core/Data/Game/PlayerConfig.cs
+++ src/Eldergrove.Engine.Core/Data/Game/PlayerConfig.cs
@@ -5,4 +5,6 @@ namespace Eldergrove.Engine.Core.Data.Game;
 public class PlayerConfig
 {
     public JsonRandomObject StartingGold { get; set; }
+
+    public List<JsonRandomObject> StartingItems { get; set; } = new();
 }
